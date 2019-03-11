a932f387f9f61d5f6796563029455d0777e0fcd5
Oskar <kogutoskar123@gmail.com>
2019-03-11 16:17:43 +0100

    Added nav mesh base

---
+++ RpgCore/Assets/Code/Mover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+    [SerializeField] private Transform _target;
+
+    private void Start() {
+        NavMeshAgent playerNavMeshAgent = GetComponent<NavMeshAgent>();
+        playerNavMeshAgent.destination = _target.transform.position;
+    }
+}
