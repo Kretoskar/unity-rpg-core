09dc27f82c234e12d10b04a9fde6e280f34e9d13
Oskar <kogutoskar123@gmail.com>
2019-03-11 17:14:01 +0100

    Added follow camera

---
+++ RpgCore/Assets/Code/FollowCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    [SerializeField] private Transform _target;
+
+    private void Update() {
+        transform.position = _target.position;
+    }
+}
