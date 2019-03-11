3ac61d9c7ca4a100c15345708f72a0268cfc94b2
Oskar <kogutoskar123@gmail.com>
2019-03-11 16:51:10 +0100

    Added camera raycast

--- RpgCore/Assets/Code/Mover.cs
+++ RpgCore/Assets/Code/Mover.cs
@@ -6,8 +6,17 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour {
     [SerializeField] private Transform _target;
 
+    private Ray _lastRay;
+
     private void Start() {
         NavMeshAgent playerNavMeshAgent = GetComponent<NavMeshAgent>();
         playerNavMeshAgent.destination = _target.transform.position;
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100);
     }
 }
