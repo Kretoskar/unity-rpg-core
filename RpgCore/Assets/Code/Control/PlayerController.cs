00e6eb1d0bf25372a326a1e55767207c81f04aa4
Oskar <kogutoskar123@gmail.com>
2019-03-13 22:30:03 +0100

    Added interaction with enemies

--- RpgCore/Assets/Code/Control/PlayerController.cs
+++ RpgCore/Assets/Code/Control/PlayerController.cs
@@ -1,18 +1,17 @@
 ﻿using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control {
     /// <summary>
-    /// Handles player input
+    /// Handles player's character interaction
     /// </summary>
     public class PlayerController : MonoBehaviour {
 
         private Mover mover;
 
         private void Update() {
-            if (Input.GetMouseButton(0)) {
-                MoveToCursor();
-            }
+            InterractWithMovement();
         }
 
         private void Start() {
@@ -20,12 +19,36 @@ namespace RPG.Control {
         }
 
         /// <summary>
+        /// Check for player input to handle movement
+        /// </summary>
+        private void InterractWithMovement() {
+            if (Input.GetMouseButton(0)) {
+                InteractWithCombat();
+                MoveToCursor();
+            }
+        }
+
+        /// <summary>
+        /// Interact with combat target
+        /// </summary>
+        private void InteractWithCombat() {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits) {
+                CombatTarget target =  hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if(Input.GetMouseButtonDown(0)) {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        /// <summary>
         /// Move character to raycast hit position
         /// </summary>
         private void MoveToCursor() {
-            Ray ray = RecalculateRaycast();
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit) {
                 mover.MoveTo(hit.point);
             }
@@ -35,7 +58,7 @@ namespace RPG.Control {
         /// Calculatre ray of the mouse click
         /// </summary>
         /// <returns> Ray of the mouse click </returns>
-        private Ray RecalculateRaycast() {
+        private Ray GetMouseRay() {
             return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
