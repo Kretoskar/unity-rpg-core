1a967e48620e879a64fc721277a430437af15a9e
Oskar <kogutoskar123@gmail.com>
2019-03-13 22:14:43 +0100

    Added namespaces for all the code

--- RpgCore/Assets/Code/Control/PlayerController.cs
+++ RpgCore/Assets/Code/Control/PlayerController.cs
@@ -1,42 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using RPG.Movement;
 
-/// <summary>
-/// Handles player input
-/// </summary>
-public class PlayerController : MonoBehaviour {
+namespace RPG.Control {
+    /// <summary>
+    /// Handles player input
+    /// </summary>
+    public class PlayerController : MonoBehaviour {
 
-    private Mover mover;
+        private Mover mover;
 
-    private void Update() {
-        if(Input.GetMouseButton(0)) {
-            MoveToCursor();
+        private void Update() {
+            if (Input.GetMouseButton(0)) {
+                MoveToCursor();
+            }
+        }
+
+        private void Start() {
+            mover = GetComponent<Mover>();
+        }
+
+        /// <summary>
+        /// Move character to raycast hit position
+        /// </summary>
+        private void MoveToCursor() {
+            Ray ray = RecalculateRaycast();
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit) {
+                mover.MoveTo(hit.point);
+            }
+        }
+
+        /// <summary>
+        /// Calculatre ray of the mouse click
+        /// </summary>
+        /// <returns> Ray of the mouse click </returns>
+        private Ray RecalculateRaycast() {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
-
-    private void Start() {
-        mover = GetComponent<Mover>();
-    }
-
-    /// <summary>
-    /// Move character to raycast hit position
-    /// </summary>
-    private void MoveToCursor() {
-        Ray ray = RecalculateRaycast();
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit) {
-            mover.MoveTo(hit.point);
-        }
-    }
-
-    /// <summary>
-    /// Calculatre ray of the mouse click
-    /// </summary>
-    /// <returns> Ray of the mouse click </returns>
-    private Ray RecalculateRaycast() {
-        return Camera.main.ScreenPointToRay(Input.mousePosition);
-    }
-
 }
--- RpgCore/Assets/Code/Movement/Mover.cs
+++ RpgCore/Assets/Code/Movement/Mover.cs
@@ -1,44 +1,43 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-/// <summary>
-/// Move character and update his animator
-/// </summary>
-public class Mover : MonoBehaviour {
-    private const string _animatorBlendValue = "ForwardSpeed";
-
-    private NavMeshAgent _navMeshAgent;
-    private Animator _animator;
-
-    private void Start() {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-        _animator = GetComponent<Animator>();
-    }
-
-    private void Update() {
-        UpdateAnimator();
-    }
-
+namespace RPG.Movement {
     /// <summary>
-    /// Move character to given destination
+    /// Move character and update his animator
     /// </summary>
-    /// <param name="destination">Where to move the character</param>
-    public void MoveTo(Vector3 destination) {
-        _navMeshAgent.destination = destination;
-    }
+    public class Mover : MonoBehaviour {
+        private const string _animatorBlendValue = "ForwardSpeed";
 
-    /// <summary>
-    /// Update animator that uses blend tree
-    /// depending on playerNavMeshAgent velocity z value
-    /// </summary>
-    private void UpdateAnimator() {
-        Vector3 velocity = _navMeshAgent.velocity;
-        // Transfer from global to local
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        _animator.SetFloat(_animatorBlendValue, speed);
+        private NavMeshAgent _navMeshAgent;
+        private Animator _animator;
+
+        private void Start() {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update() {
+            UpdateAnimator();
+        }
+
+        /// <summary>
+        /// Move character to given destination
+        /// </summary>
+        /// <param name="destination">Where to move the character</param>
+        public void MoveTo(Vector3 destination) {
+            _navMeshAgent.destination = destination;
+        }
+
+        /// <summary>
+        /// Update animator that uses blend tree
+        /// depending on playerNavMeshAgent velocity z value
+        /// </summary>
+        private void UpdateAnimator() {
+            Vector3 velocity = _navMeshAgent.velocity;
+            // Transfer from global to local
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            _animator.SetFloat(_animatorBlendValue, speed);
+        }
     }
 }
