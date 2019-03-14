43884800cc6cfd6f290bf2cde4cb2c5067c7835a
Oskar <kogutoskar123@gmail.com>
2019-03-15 00:24:28 +0100

    Added death animation

--- RpgCore/Assets/Code/Combat/Health.cs
+++ RpgCore/Assets/Code/Combat/Health.cs
@@ -7,15 +7,26 @@ namespace RPG.Combat {
     /// Handles health of characters
     /// </summary>
     public class Health : MonoBehaviour {
-        [SerializeField] private float health = 100f;
+        [SerializeField] private float _healthPoints = 100f;
+
+        private bool _isDead = false;
 
         /// <summary>
         /// Take damage from a fighter
         /// </summary>
         /// <param name="damage">Damage to take</param>
         public void TakeDamage(float damage) {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            _healthPoints = Mathf.Max(_healthPoints - damage, 0);
+            print(_healthPoints);
+            if (_healthPoints == 0) {
+                Die();
+            }
+        }
+
+        private void Die() {
+            if (_isDead) return;
+            _isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
         }
     }
 }
