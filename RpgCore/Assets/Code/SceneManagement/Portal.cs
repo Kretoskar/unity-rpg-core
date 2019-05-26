92899a67b2e32a27f3a1bd0b0d753cae5ac74bf8
Oskar <kogutoskar123@gmail.com>
2019-05-26 22:03:33 +0200

    Added a portal prefab

---
+++ RpgCore/Assets/Code/SceneManagement/Portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace RPG.SceneManagement {
+    public class Portal : MonoBehaviour {
+
+        [SerializeField]
+        private int _indexOfSceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other) {
+            print("xd");
+            if (other.tag == "Player") {
+                print("xdp");
+                SceneManager.LoadScene(_indexOfSceneToLoad);
+            }
+        }
+    }
+}
