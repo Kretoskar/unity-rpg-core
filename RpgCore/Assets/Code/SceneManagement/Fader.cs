2b583a59ec40248c1fef3e31b1a49124516fba64
Oskar <kogutoskar123@gmail.com>
2019-05-27 23:06:01 +0200

    Fixed multiple portals in one scene, added Fader.cs

---
+++ RpgCore/Assets/Code/SceneManagement/Fader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement {
+    public class Fader : MonoBehaviour {
+
+        private CanvasGroup _canvasGroup;
+
+        private void Awake() {
+            _canvasGroup = GetComponent<CanvasGroup>();
+            _canvasGroup.alpha = 0;
+        }
+
+        private IEnumerator FadeOutIn() {
+            yield return FadeOut(1f);
+            yield return FadeIn(1f);
+        }
+
+        public IEnumerator FadeOut(float time) {
+            while(_canvasGroup.alpha < 1) {
+                _canvasGroup.alpha += Time.deltaTime / time;
+                yield return null; // waits one frame
+            }
+        }
+
+        public IEnumerator FadeIn(float time) {
+            while(_canvasGroup.alpha > 0) {
+                _canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null; // waits one frame
+            }
+        }
+    }
+}
--- RpgCore/Assets/Code/SceneManagement/Portal.cs
+++ RpgCore/Assets/Code/SceneManagement/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,15 +8,53 @@ using UnityEngine.SceneManagement;
 namespace RPG.SceneManagement {
     public class Portal : MonoBehaviour {
 
+        enum DestinationIdentifier {
+            A, B, C, D, E
+        }
+
         [SerializeField]
         private int _indexOfSceneToLoad = -1;
+        [SerializeField]
+        private Transform _spawnPoint = null;
+        [SerializeField]
+        private DestinationIdentifier _destination;
 
         private void OnTriggerEnter(Collider other) {
-            print("xd");
             if (other.tag == "Player") {
-                print("xdp");
-                SceneManager.LoadScene(_indexOfSceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition() {
+            if(_indexOfSceneToLoad < 0) {
+                Debug.LogError("Scene to load not set");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(_indexOfSceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal) {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal._spawnPoint.position;
+            player.transform.rotation = otherPortal._spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal() {
+            foreach (Portal portal in FindObjectsOfType<Portal>()) {
+                if (portal == this)
+                    continue;
+                if (portal._destination != _destination)
+                    continue;
+                return portal;
+            }
+            return null;
         }
     }
 }
