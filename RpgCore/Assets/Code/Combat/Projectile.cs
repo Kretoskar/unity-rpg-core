ef0aea209ebbc26db466afb0ba9bace1ff7b8f87
Oskar <kogutoskar123@gmail.com>
2019-06-20 17:34:34 +0200

    Added an arrow projectile

---
+++ RpgCore/Assets/Code/Combat/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat {
+    public class Projectile : MonoBehaviour {
+
+        [SerializeField] private Transform _target = null;
+        [SerializeField] private float _speed = 1;
+        [SerializeField] private float _howHighToAim = 1.1f;
+
+        private void Update() {
+            Shoot();
+        }
+
+        private void Shoot() {
+            if (_target == null)
+                return;
+            transform.LookAt(GetAimLocation());
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+
+        private Vector3 GetAimLocation() {
+            CapsuleCollider target = _target.GetComponent<CapsuleCollider>();
+            if (target == null)
+                return target.transform.position;
+            return target.transform.position + Vector3.up * target.height / _howHighToAim;
+        }
+    }
+}
