5639e4bb4f0e7956661cc972131a1c60a81bc880
Oskar <kogutoskar123@gmail.com>
2019-09-24 18:47:33 +0200

    Added gloabal stats info change on player stats change

---
+++ RpgCore/Assets/Code/Stats/GlobalStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats {
+    public static class GlobalStats {
+        public static float AttackDamage(float weaponDamage, float strength) {
+            return strength * 10 + weaponDamage * 2;
+        }
+
+        public static float MagicDamage(float power) {
+            return power * 10;
+        }
+
+        public static float Health(float durability) {
+            return durability * 100;
+        }
+
+        public static float Armor(float armor) {
+            return 50 + armor;
+        }
+    }
+}
---
+++ RpgCore/Assets/Code/UI/InventoryUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RPG.Stats;
+using RPG.Combat;
+using RPG.Control;
+
+namespace RPG.UI {
+    public class InventoryUI : MonoBehaviour {
+        [SerializeField]
+        private Text _damageText = null;
+
+        [SerializeField]
+        private Text _powerText = null;
+
+        [SerializeField]
+        private Text _armorText = null;
+
+        private PlayerStats _playerStats;
+        private Fighter _playerFighter;
+
+        private void Start() {
+            _playerStats = PlayerStats.Instance;
+            _playerFighter = PlayerController.Instance.GetComponent<Fighter>(); 
+            _playerStats.DurabilityChanged += UpdateUI;
+            _playerStats.StrengthChanged += UpdateUI;
+            _playerStats.PowerChanged += UpdateUI;
+        }
+
+        private void UpdateUI() {
+            _damageText.text = GlobalStats.AttackDamage(_playerFighter.CurrentWeapon.GetDamage(), _playerStats.Strength).ToString();
+            _powerText.text = GlobalStats.MagicDamage(_playerStats.Power).ToString();
+            _armorText.text = "0";
+        }
+
+    }
+}
